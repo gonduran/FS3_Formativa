4396300df29da3457871aa55bed73277fa556220
Gonzalo Durán <gon.duran@duocuc.cl>
2024-11-11 21:03:19 -0300

    Entrega semana 4 fullstack 3

--- biblioteca-ms-libros/src/main/java/com/example/biblioteca_ms_libros/config/WebConfig.java
+++ biblioteca-ms-libros/src/main/java/com/example/biblioteca_ms_libros/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") 
-                .allowedOrigins("http://localhost:80")
+                .allowedOrigins("http://frontend:80")
                 .allowedMethods("GET", "POST", "PUT", "DELETE") 
                 .allowedHeaders("*")
                 .allowCredentials(true);
