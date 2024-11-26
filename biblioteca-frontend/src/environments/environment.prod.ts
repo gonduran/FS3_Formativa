2b4e3ba0f54ec19de39a8237375626236d46188f
Gonzalo Durán <gon.duran@duocuc.cl>
2024-11-25 21:25:39 -0300

    Entrega semana 6 fullstack 3

--- biblioteca-frontend/src/environments/environment.prod.ts
+++ biblioteca-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:8091/libros'  // URL del backend en Docker
+    apiUrl: 'http://ip172-18-0-18-ct2gujiim2rg008ls7sg-8091.direct.labs.play-with-docker.com/libros'  // URL del backend en Docker http://localhost:8091/libros
   };
